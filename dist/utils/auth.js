ce3a7614cb039a889138c554a93b0bb8770ee8d7
CENWACHUKWU <CENWACHUKWUU@GMAIL.COM>
2020-02-27 14:05:41 -0500

    added comments for token

--- dist/utils/auth.js
+++ dist/utils/auth.js
@@ -13,13 +13,22 @@ var _jsonwebtoken = _interopRequireDefault(require("jsonwebtoken"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+// authetication is controlling if an incoming request can proceed or not
+// authorization is controlling if an authenticated request has the correct permissions to access a resource
+// jwt authentications are basically tokens passed to check auth on the server. so it's a bearer token strategy that allows the api to be statless with user auth
+// newToken function takes in User from the user.model and creates a new json web token based on the user.id
+// signed with the correct secerts and xpiration time
+// user goes in, token come out
 const newToken = user => {
   return _jsonwebtoken.default.sign({
     id: user.id
   }, _config.default.secrets.jwt, {
     expiresIn: _config.default.secrets.jwtExp
   });
-};
+}; // verifyToken function does the opposite of newToken. So given a token, it will verify that the token was created with the same secret from the same server
+// an it will return a payload and in this case it would be a user
+// token goes in user comes out
+
 
 exports.newToken = newToken;
 
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,22 @@
+// authetication is controlling if an incoming request can proceed or not
+// authorization is controlling if an authenticated request has the correct permissions to access a resource
+// jwt authentications are basically tokens passed to check auth on the server. so it's a bearer token strategy that allows the api to be statless with user auth
 import config from '../config'
 import { User } from '../resources/user/user.model'
 import jwt from 'jsonwebtoken'
 
+// newToken function takes in User from the user.model and creates a new json web token based on the user.id
+// signed with the correct secerts and xpiration time
+// user goes in, token come out
 export const newToken = user => {
   return jwt.sign({ id: user.id }, config.secrets.jwt, {
     expiresIn: config.secrets.jwtExp
   })
 }
 
+// verifyToken function does the opposite of newToken. So given a token, it will verify that the token was created with the same secret from the same server
+// an it will return a payload and in this case it would be a user
+// token goes in user comes out
 export const verifyToken = token =>
   new Promise((resolve, reject) => {
     jwt.verify(token, config.secrets.jwt, (err, payload) => {
